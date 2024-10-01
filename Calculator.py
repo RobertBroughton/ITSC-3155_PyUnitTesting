cb1d9135db81d78f3907b303368429081419ef8b
Robert <bobsaidhi01@gmail.com>
2024-10-01 12:54:27 -0400

    Added Basic Calculator Methods

---
+++ Calculator.py
@@ -0,0 +1,16 @@
+class Calculator:
+    @staticmethod
+    def add(a, b):
+        pass
+
+    @staticmethod
+    def subtract(a, b):
+        pass
+
+    @staticmethod
+    def multiply(a, b):
+        pass
+
+    @staticmethod
+    def divide(a, b):
+        pass
