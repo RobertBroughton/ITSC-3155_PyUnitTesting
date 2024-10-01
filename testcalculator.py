d4722a71d14ea8c55b3cdd8c356a19f2b3bc7355
Robert <bobsaidhi01@gmail.com>
2024-10-01 13:28:15 -0400

    Added More Tests

--- testcalculator.py
+++ testcalculator.py
@@ -4,12 +4,16 @@ from calculator import Calculator
 class TestCalculator(unittest.TestCase):
     def test_add(self):
         self.assertEqual(Calculator.add(2, 2), 4)
+        self.assertEqual(Calculator.add(1, 1), 2)
     def test_subtract(self):
         self.assertEqual(Calculator.subtract(2, 2), 0)
+        self.assertEqual(Calculator.subtract(1, 1), 0)
     def test_multiply(self):
         self.assertEqual(Calculator.multiply(2, 2), 4)
+        self.assertEqual(Calculator.multiply(1, 1), 1)
     def test_divide(self):
         self.assertEqual(Calculator.divide(2, 2), 1)
+        self.assertEqual(Calculator.divide(4, 2), 2)
 
 if __name__ == '__main__':
     unittest.main()
